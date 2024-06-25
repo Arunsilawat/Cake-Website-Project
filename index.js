c25a5ad43a75b34e415e49db6d1af12e563c7617
Arun silawat <arunsilawat722362@gmail.com>
2024-06-25 14:22:47 +0530

    Multi Page Project

---
+++ index.js
@@ -0,0 +1,63 @@
+function change(){
+    document.getElementById("cng").src ="https://thumbs.dreamstime.com/b/chocolate-birthday-cake-decorated-colorful-desserts-decorated-chocolate-birthday-cake-decorated-colorful-desserts-302948678.jpg";
+}
+function change2(){
+    document.getElementById("cng").src ="https://thumbs.dreamstime.com/b/fruit-chocolate-birthday-cake-white-background-fruit-chocolate-birthday-cake-white-background-113735538.jpg";
+}
+// ======================================================================================
+function changes(){
+    document.getElementById("cng2").src ="https://www.shutterstock.com/image-photo/chocolate-cake-berries-600nw-394680466.jpg";
+}
+function changes1(){
+    document.getElementById("cng2").src ="https://img.freepik.com/premium-photo/birthday-cake-realistic-photograph-white-background_300636-1825.jpg";
+}
+// ======================================================================================
+
+function changet(){
+    document.getElementById("cng3").src ="https://img.freepik.com/free-photo/photo-decorated-birthday-cake-light-background_125540-3927.jpg";
+}
+function changet1(){
+    document.getElementById("cng3").src ="https://st.depositphotos.com/1068095/3276/i/450/depositphotos_32761307-stock-photo-cake.jpg";
+}
+// ======================================================================================
+
+function changef(){
+    document.getElementById("cng4").src ="https://img.freepik.com/free-photo/birthday-chocolate-cake-with-colored-candles-blue-background-ai-generative_123827-24052.jpg?size=626&ext=jpg&ga=GA1.1.1269040533.1710288000&semt=ais";
+}
+function changef1(){
+    document.getElementById("cng4").src ="https://static4.depositphotos.com/1001719/350/i/450/depositphotos_3506392-stock-photo-chocolate-cake.jpg";
+}
+
+// ===============================end good test 1 =======================================
+function cha(){
+    document.getElementById("chan1").src ="https://mrbrownbakery.com/image/images/GJ7uCwGiteTF24HTWBclkziVTdhpQeZWH23MvQfq.jpeg?p=full";
+}
+function cha1(){
+    document.getElementById("chan1").src ="https://img.freepik.com/premium-photo/chocolate-cake-with-with-berries_127425-305.jpg";
+}
+
+
+
+function chas(){
+    document.getElementById("chan2").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyPIKbpuG_J1iR2tNPQOhqIMcy3Dd_pBYFzQ&usqp=CAU";
+}
+function chas1(){
+    document.getElementById("chan2").src ="https://r2.erweima.ai/imgcompressed/compressed_4d3c23eb7a1fb57cd5de755c00126b59.webp";
+}
+
+
+function chat(){
+    document.getElementById("chan3").src ="https://global.discourse-cdn.com/smallgiantgames/original/4X/c/d/8/cd89c27fc0c0960cafc4176473070849f6ff85f2.jpeg";
+}
+function chat1(){
+    document.getElementById("chan3").src ="https://st4.depositphotos.com/10614052/25239/i/450/depositphotos_252391082-stock-photo-sweet-chocolate-cake-on-wooden.jpg";
+}
+
+
+
+function chaf(){
+    document.getElementById("chan4").src ="https://t4.ftcdn.net/jpg/03/08/40/43/360_F_308404380_9WKXklvcqeT8q4kNSVTtfUNwmcBAlPyk.jpg";
+}
+function chaf1(){
+    document.getElementById("chan4").src ="https://img.lovepik.com/photo/48005/2324.jpg_wh300.jpg";
+}
